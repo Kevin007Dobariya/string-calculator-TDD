89b900183656c620253e2395dfe1ac6d652e3134
kevin <dobariya007kevin@gmail.com>
2025-08-06 17:47:36 +0530

    Add logic to handle empty string

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,11 +1,8 @@
 //create a class StringCalculator for adding numbers
 class StringCalculator {
-   add(String numbers) {
-    try {
-      int number = int.parse(numbers);
-      return number;
-    } catch (e) {
-      print(e);
+  add(String numbers) {
+    if (numbers.isEmpty) {
+      return 0;
     }
   }
 }
