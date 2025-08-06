df666fa5c053960df9b77f8d95e1398af4968eba
kevin <dobariya007kevin@gmail.com>
2025-08-06 21:09:56 +0530

    Add logic to identify negative numbers and thow exception if found and refactored custom delimiter logic

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,26 +4,29 @@ class StringCalculator {
     if (numbers.isEmpty) {
       return 0;
     }
-    String delimiter = '';
 
     //find Custom delimiter
     if (numbers.startsWith('//')) {
-      delimiter = numbers.substring(2, 3);
-    }
+      String delimiter = numbers.substring(2, 3);
       // clean the input string
-      numbers = numbers
-          .replaceAll('\n', ',')
-          .replaceAll(delimiter, ',')
-          .replaceAll("//", ',');
-
-      // handle strings with and comma as delimiters
-      List<String> parsedNumbers = numbers.split(',');
-      // remove empty strings from the list
-      parsedNumbers.removeWhere((item) => item.isEmpty);
-      int sum = 0;
-      for (String number in parsedNumbers) {
-        sum += int.parse(number);
-      }
-      return sum;
+      numbers = numbers.replaceAll(delimiter, ',').replaceAll("//", ',');
     }
+
+    // replace \n with ,
+    numbers = numbers.replaceAll('\n', ',');
+    // handle strings with and comma as delimiters
+    List<String> parsedNumbers = numbers.split(',');
+    // remove empty strings from the list
+    parsedNumbers.removeWhere((item) => item.isEmpty);
+    List<int> negatives =
+        parsedNumbers.map(int.parse).where((n) => n < 0).toList();
+    if (negatives.isNotEmpty) {
+      throw Exception('negative numbers not allowed: ${negatives.join(',')}');
+    }
+    int sum = 0;
+    for (String number in parsedNumbers) {
+      sum += int.parse(number);
+    }
+    return sum;
   }
+}
